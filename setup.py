789377d289cf3805729b9ec375d22b521f9ee153
Mattia Procopio <promat85@gmail.com>
2018-09-25 11:57:15 +0200

    Prepeare fo CI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="aioscheduler",
+    version='0.1.0',
+    description="A Thread scheduler to dispatch periodic tasks to other event loops",
+    long_description="Visit https://github.com/MattBlack85/aio-scheduler for more information.",
+    packages=[
+        "aioscheduler",
+    ],
+    install_requires=[],
+    python_requires=">=3.5",
+    include_package_data=True,
+)
